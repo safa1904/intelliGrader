ea9a52c94e3ba63b33bbcecbfe7f906a25dc4566
Saphia Mohamed <100108435+safa1904@users.noreply.github.com>
2024-02-16 09:21:54 -0700

    first commit

---
+++ app/src/main/java/com/example/grader/ui/gallery/TermViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.grader.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TermViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is Term Fragment"
+    }
+    val text: LiveData<String> = _text
+}
